af7fe5089e791bb789164df750f18499a84e9bec
punnlert <punnlert@users.noreply.github.com>
2024-11-22 13:31:48 +0000

    deploy: 240943c5b51deab762db4d2b66e702fcaae5e647

--- _app/immutable/chunks/12.aa24f5c7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/(waves)-page.svelte.46149716.js";export{t as component};
--- _app/immutable/chunks/4.371a999d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/(blog-article)-baobal-page.md.c4e24181.js";export{t as component};
