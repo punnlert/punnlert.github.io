d2491edc86c7e14aae203a172abf2fb6a752330f
punnlert <punnlert@users.noreply.github.com>
2024-11-28 02:08:36 +0000

    deploy: 129fa59c34c524d055b85afd3c8f37e41228ea43

--- _app/immutable/chunks/4.1fb4a0b4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/(blog-article)-baobal-page.md.63835f8e.js";export{t as component};
---
+++ _app/immutable/chunks/4.d780f9d9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/(blog-article)-baobal-page.md.e6e6f484.js";export{t as component};
