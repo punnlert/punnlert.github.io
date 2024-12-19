d016b7d2eaec7f31414f9299bc20eed024ee813d
20210770 <punnlert@kaist.ac.kr>
2024-12-20 02:32:01 +0900

    added transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import '$lib/scss/global.scss';
 	import Analytics from '$lib/data/Analytics.svelte';
+	import ViewTransition from '$lib/components/atoms/Navigation.svelte'
 </script>
 
 <Analytics />
+<ViewTransition />
 
 <slot />
