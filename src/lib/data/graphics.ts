7ffbb6c8a62d13a2d44780af43ce71e74359517f
20210770 <punnlert@kaist.ac.kr>
2025-01-02 16:20:42 +0700

    masonry with all graphic designs

--- src/lib/data/graphics.ts
+++ src/lib/data/graphics.ts
@@ -2,11 +2,6 @@ import type { Graphics } from '$lib/utils/types';
 
 export default [
 	{
-		name: 'Hello Eoeun Festival',
-		description: 'This is a festival that...',
-		image: 'images/graphics/eoeun-hello.png'
-	},
-	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
 		image: 'images/graphics/ChronicleWorkshop-s2024.png'
@@ -25,5 +20,40 @@ export default [
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
 		image: 'images/graphics/KastleTournament-s2024.png'
+	},
+	{
+		name: 'Chronicle Creative Writing Workshop Spring 2024',
+		description: 'This is a festival that...',
+		image: 'images/graphics/TSAKLogo-summer2024.png'
+	},
+	{
+		name: 'Chronicle Creative Writing Workshop Spring 2024',
+		description: 'This is a festival that...',
+		image: 'images/graphics/KastleEvent-s2024.png'
+	},
+	{
+		name: 'Chronicle Creative Writing Workshop Spring 2024',
+		description: 'This is a festival that...',
+		image: 'images/graphics/KastleRecruit-f2024.png'
+	},
+	{
+		name: 'Chronicle Creative Writing Workshop Spring 2024',
+		description: 'This is a festival that...',
+		image: 'images/graphics/ChronicleRecruit-f2024.png'
+	},
+	{
+		name: 'Chronicle Creative Writing Workshop Spring 2024',
+		description: 'This is a festival that...',
+		image: 'images/graphics/ChronicleWorkshop-f2024.png'
+	},
+	{
+		name: 'Chronicle Creative Writing Workshop Spring 2024',
+		description: 'This is a festival that...',
+		image: 'images/graphics/KastleHoodie-f2024.jpg'
+	},
+	{
+		name: 'Chronicle Creative Writing Workshop Spring 2024',
+		description: 'This is a festival that...',
+		image: 'images/graphics/KastleHoodies-f2024.png'
 	}
 ] as Graphics[];
