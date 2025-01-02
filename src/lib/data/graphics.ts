1013380ee50615d5ac288f1b110ce1b9f26c6408
20210770 <punnlert@kaist.ac.kr>
2025-01-02 20:03:22 +0700

    fix path issue in graphics

--- src/lib/data/graphics.ts
+++ src/lib/data/graphics.ts
@@ -4,56 +4,56 @@ export default [
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/ChronicleWorkshop-s2024.png'
+		image: '/images/graphics/ChronicleWorkshop-s2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/ChronicleOpenevent-s2024.png'
+		image: '/images/graphics/ChronicleOpenevent-s2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/ChronicleHoodies-s2024.png'
+		image: '/images/graphics/ChronicleHoodies-s2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/KastleTournament-s2024.png'
+		image: '/images/graphics/KastleTournament-s2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/TSAKLogo-summer2024.png'
+		image: '/images/graphics/TSAKLogo-summer2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/KastleEvent-s2024.png'
+		image: '/images/graphics/KastleEvent-s2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/KastleRecruit-f2024.png'
+		image: '/images/graphics/KastleRecruit-f2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/ChronicleRecruit-f2024.png'
+		image: '/images/graphics/ChronicleRecruit-f2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/ChronicleWorkshop-f2024.png'
+		image: '/images/graphics/ChronicleWorkshop-f2024.png'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/KastleHoodie-f2024.jpg'
+		image: '/images/graphics/KastleHoodie-f2024.jpg'
 	},
 	{
 		name: 'Chronicle Creative Writing Workshop Spring 2024',
 		description: 'This is a festival that...',
-		image: 'images/graphics/KastleHoodies-f2024.png'
+		image: '/images/graphics/KastleHoodies-f2024.png'
 	}
 ] as Graphics[];
