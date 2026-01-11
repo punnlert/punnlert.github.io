4d0825658994bfc6e7227f8cde601cb9e8b4306d
punnlert <punnlert@users.noreply.github.com>
2026-01-11 11:15:38 +0000

    deploy: 0443bd11bee02b601424189db99c37ab1ccaf652

--- _app/immutable/chunks/navigation.868690c0.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./singletons.c4a3ac00.js";const o=a("before_navigate"),t=a("on_navigate");export{o as b,t as o};
---
+++ _app/immutable/chunks/navigation.960ad41d.js
@@ -0,0 +1,1 @@
+import{j as a}from"./singletons.4143386c.js";const o=a("before_navigate"),t=a("on_navigate");export{o as b,t as o};
