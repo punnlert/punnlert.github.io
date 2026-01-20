31ccd2b9b91248db14dd67b9168d1df1f952866e
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-01-21 02:23:12 +0900

    edit about

--- src/lib/components/organisms/About.svelte
+++ src/lib/components/organisms/About.svelte
@@ -5,6 +5,9 @@
 </script>
 
 <section id="about">
+	<div class="image">
+		<Image src="/images/profile.jpeg" alt="Punn with a cat" />
+	</div>
 	<div class="info">
 		<h2>
 			<!-- This is a catchy headline that
@@ -12,12 +15,23 @@
 			Hi! I'm Punn Lertjaturaphat.
 		</h2>
 		<p>
-			A designer who loves to create meaningful interaction. I always keep looking for a way to mix
-			fun physical experience into an otherwise mundane digital task we perform everyday.
+			<!-- A designer who loves to create meaningful interaction. I always keep looking for a way to mix -->
+			<!-- fun physical experience into an otherwise mundane digital task we perform everyday. -->
+			I am a KAIST undergraduate majoring in Industrial Design and Computer Science. Previously working
+			with Professor Andrea Bianchi in <a href="https://make.kaist.ac.kr/"> Make Lab </a>
 		</p>
 		<p>
-			Currently majoring in Industrial Design and Computer Science at
-			<a href="https://www.kaist.ac.kr/en/html/kaist/01.html">KAIST</a>
+			My motivation in research and in design lies in creating intellectually-munchable interaction.
+			I strive to make tools that help people think and see. Tools that let people be in control.
+			Interaction that enhances creative process not by making it faster or easier but by helping
+			the people understand it better.
+			<!-- As a passionate maker, I believe that the connection with your craft lies as much in the -->
+			<!-- process as in the final result. While faster and easier tools are important, I think focusing on it too much makes the making boring -->
+		</p>
+		<p>
+			Weekends and end of the days I am going to either be making weird sounds on my synths or
+			bouldering. I also enjoy talking to shop keepers and lunch ladies, holding up the lines on
+			busy days.
 		</p>
 		<div class="socials">
 			<Socials />
@@ -31,9 +45,8 @@
 	#about {
 		position: relative;
 		display: grid;
-		grid-template-columns: 1fr;
+		grid-template-columns: 1fr 1fr;
 		align-items: center;
-		justify-content: space-between;
 		padding-top: 50px;
 		padding-bottom: 50px;
 
@@ -102,7 +115,7 @@
 				width: 100%;
 				height: 100%;
 				max-height: 400px;
-				object-fit: cover;
+				object-fit: contain;
 			}
 		}
 	}
