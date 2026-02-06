c333ef0a8e1cb0812700c6e569d262e3c08568a6
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-02-07 01:49:19 +0900

    add a marquee or something announcing my paper haha

---
+++ src/lib/components/organisms/Announcement.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Marquee from '$lib/components/atoms/Marquee.svelte';
+</script>
+
+<div class="announcement">
+	<Marquee pauseOnHover={true} gap={'20px'} speed={50}>
+		{#each Array(7).fill(0) as _, i (i)}
+			<div class="announcement-text">
+				<h4>
+					<a href="/publications/wireway-2026"> New Paper </a>to Appear at CHI2026!
+				</h4>
+			</div>
+		{/each}
+	</Marquee>
+</div>
+
+<style lang="scss">
+	.announcement {
+		padding-top: 10px;
+		padding-bottom: 10px;
+		background-color: var(--color--primary);
+	}
+</style>
--- src/routes/(waves)/+page.svelte
+++ src/routes/(waves)/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import Hero from '$lib/components/organisms/Hero.svelte';
 	import About from '$lib/components/organisms/About.svelte';
 	import RecentPosts from '$lib/components/organisms/RecentPosts.svelte';
 	import RecentPublications from '$lib/components/organisms/RecentPublications.svelte';
-	import Features from '$lib/components/organisms/Features.svelte';
 	import RecentEvents from '$lib/components/organisms/RecentEvents.svelte';
 	import type { Feature, BlogPost, Publication, Event } from '$lib/utils/types';
+	import Announcement from '$lib/components/organisms/Announcement.svelte';
 
 	export let data: {
 		features: Feature[];
@@ -17,8 +16,10 @@
 	let { features, posts, publications, events } = data;
 </script>
 
+<Announcement />
 <div class="container">
 	<!-- <Hero /> -->
+
 	<About />
 	{#if events && events.length > 0}
 		<RecentEvents {events} />
