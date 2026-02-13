2017d26f36d90302cf6bc6ebf66140d8c01c8b3e
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-02-13 17:13:58 +0900

    fix marquee overflow

--- src/lib/components/organisms/Announcement.svelte
+++ src/lib/components/organisms/Announcement.svelte
@@ -15,9 +15,28 @@
 </div>
 
 <style lang="scss">
+	@import '$lib/scss/_mixins.scss';
 	.announcement {
 		padding-top: 10px;
 		padding-bottom: 10px;
 		background-color: var(--color--primary);
+		overflow: hidden;
+		width: 100vw;
+
+		// @include for-iphone-se {
+		// 	margin-left: 0px;
+		// }
+		//
+		// @include for-tablet-portrait-up {
+		// 	margin-left: -20px;
+		// }
+		//
+		// @include for-tablet-landscape-up {
+		// 	margin-left: calc(-30px - 60px);
+		// }
+		//
+		// @include for-desktop-up {
+		// 	margin-left: calc((1280px - 60px - 100vw) / 2);
+		// }
 	}
 </style>
