9f5d181755e8132c9a090452fd8ac8cb830ea7ad
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-07-29 15:09:12 +0900

    smooth scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import '$lib/scss/global.scss';
+	import 'lenis/dist/lenis.css';
 	import Analytics from '$lib/data/Analytics.svelte';
 	import ViewTransition from '$lib/components/atoms/Navigation.svelte'
+    	import Lenis from 'lenis';
+	import { onMount } from 'svelte';
+
+    onMount(() => {
+		const lenis = new Lenis({
+			autoRaf: true,
+			autoToggle: true,
+			anchors: true,
+			allowNestedScroll: true,
+			stopInertiaOnNavigate: true
+		});
+
+		return () => lenis.destroy();
+	});
 </script>
 
 <Analytics />
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import Icons from 'unplugin-icons/vite';
 import { partytownVite } from '@qwik.dev/partytown/utils';
 import path from 'path';
 import { execSync } from 'child_process';
@@ -15,7 +16,7 @@ function getLastCommitDate() {
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), partytownVite()],
+	plugins: [sveltekit(), Icons({ compiler: 'svelte' }), partytownVite()],
 	resolve: {
 		alias: {
 			$routes: path.resolve('./src/routes')
