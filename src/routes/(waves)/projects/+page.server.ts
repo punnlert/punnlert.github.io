e0ffc8f0a200babac127b55bbd8bd3bdc3b58836
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-08-04 01:25:24 +0900

    try not prerendering the page

--- src/routes/(waves)/projects/+page.server.ts
+++ src/routes/(waves)/projects/+page.server.ts
@@ -1,11 +1,12 @@
 import { filteredPosts } from '$lib/data/blog-posts';
 import type { PageServerLoad } from './$types';
+export const prerender = false;
 
-export const load: PageServerLoad = async ({url}) => {
-    const filter = url.searchParams.getAll('filter');
+export const load: PageServerLoad = async ({ url }) => {
+	const filter = url.searchParams.getAll('filter');
 	return {
 		posts: filteredPosts,
 		allTags: [...new Set(filteredPosts.flatMap((post) => post.tags ?? []))].sort(),
-        filter
+		filter
 	};
 };
